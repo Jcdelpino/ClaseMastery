a6d811a18add8dd1ccf2e05d1eb125c0ce803de4
jcdelpino <jcdelpino@hotmail.com>
2023-04-12 15:30:12 -0400

    Contrato MasteryToken

--- contracts/MasteryToken.sol
+++ contracts/MasteryToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MasteryToken is ERC20 {
     constructor() ERC20("MasteryToken", "MTK") {
-        _mint(msg.sender, 100000000000000000000);
+        _mint(msg.sender, 1000000000000000000000);
     }
 }
